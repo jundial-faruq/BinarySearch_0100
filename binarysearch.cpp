423dcbe9e653db281ac97fffd52ac7c960fd26dd
JundiAlfaruq <jundialfaruq6@gmail.com>
2025-05-07 15:12:07 +0700

    Membuat fungsi jika lebih dari sepuluh elemen maka tidak bisa

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -11,7 +11,16 @@ void inpu()
     {
         cout << " Masukan banyaknya elemen Array (maksimal 10) : ";
         cin >> nPanjang;
+        if (nPanjang <= 10)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\n[!] Jumlah elem tidak boleh lebih dari 10, silahkan coba lagi.\n";
+        }
         
+
     }
     
 }
